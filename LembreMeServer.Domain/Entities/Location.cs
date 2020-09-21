cc2005c134d98ed1524e30991fc1d3afcbf30035
Vitor de Souza <vitordesolza@gmail.com>
2020-09-21 14:54:53 -0300

    Estrutura do projeto

---
+++ LembreMeServer.Domain/Entities/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LembreMeServer.Domain.Entities
+{
+    public class Location
+    {
+        public long ID { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string Neighborhood { get; set; }
+        public string City { get; set; }
+        public string FederativeUnit { get; set; }
+    }
+}
---
+++ LembreMeServer.Domain/Entities/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LembreMeServer.Domain.Entities
+{
+    public class Task
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
